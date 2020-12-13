bc82b1c68f239829aeb375b3765a9def2dc5e7c2
Ramil Aliyev <ramil.aliyev.23@mail.ru>
2020-12-13 18:14:48 +0400

    Третья неделя, исправления

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 const body = document.body;
 const hamburger = document.querySelector('.hamburger');
-const menuList = document.querySelector('.menu__list');
+const overlay = document.querySelector('.overlay');
 
+const menuLinks = document.querySelectorAll('.menu__link');
+
+menuLinks.forEach( e => {
+    e.addEventListener('click', toggleMenu);
+});
+
+function toggleMenu () {
+    hamburger.classList.toggle('hamburger--active');
+    overlay.classList.toggle('overlay--active');
+    body.classList.toggle('body--active-menu');
+}; 
+
+hamburger.addEventListener('click', toggleMenu);
+
+
+// hamburger.addEventListener('click', (e) => {
+//     clicked = !clicked;
+
+//     if (clicked) {
+//         hamburger.classList.add('hamburger--active');
+//         menuList.classList.add('menu__list--active');
+//         body.classList.add('body--active-menu');
+//     } else {
+//         hamburger.classList.remove('hamburger--active');
+//         menuList.classList.remove('menu__list--active');
+//         body.classList.remove('body--active-menu');
+//     }
+// });
 
 var clicked = false;
 
-hamburger.addEventListener('click', (e) => {
-    clicked = !clicked;
-
-    if (clicked) {
-        hamburger.classList.add('hamburger--active');
-        menuList.classList.add('menu__list--active');
-        body.classList.add('body--active-menu');
-    } else {
-        hamburger.classList.remove('hamburger--active');
-        menuList.classList.remove('menu__list--active');
-        body.classList.remove('body--active-menu');
-    }
-});
-
-
-
-const goodsOpenBtn = document.querySelector('#goods__composition-btn-icon');
+const goodsOpenBtn = document.querySelector('#goods__composition-btn');
 const goodsComponentList = document.querySelector('#goods__composition-block');
 
 goodsOpenBtn.addEventListener('click', (e) => {
